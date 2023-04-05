6529c119b895e8f6bf2cff0d30f3ca79d0b1c3f1
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-05 17:21:01 +0900

    インデントの修正を行いました。

--- Ensyu1_2/src/Ensyu1_2/Ensyu1_2.java
+++ Ensyu1_2/src/Ensyu1_2/Ensyu1_2.java
@@ -6,15 +6,15 @@ package Ensyu1_2;
  *  作成日:2023/04/05
  */
 public class Ensyu1_2 {
-/*
- * 関数名:main
- * 概要:各行に一文字ずつ表示する
- * 引数:なし
- * 戻り値:なし
- * 作成者:城所佑奈
- * 作成日:2023/04/05
- * */
-public static void main (String[] args){
+	/*
+	 * 関数名:main
+	 * 概要:各行に一文字ずつ表示する
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:城所佑奈
+	 * 作成日:2023/04/05
+	 * */
+	public static void main (String[] args){
 //		標準出力ストリームを用いて名前を表示する
 //		ここでは「println」ではなく「\n」をもちいて改行を行っている。
 		System.out.print("城\n所\n佑\n奈");
