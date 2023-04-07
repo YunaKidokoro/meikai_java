54e6d58f9825a99719adcd75ada4aac2c3665eff
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-07 17:29:27 +0900

    変数の命名方法を修正

--- Ensyu2_2/src/Ensyu2_2/Ensyu2_2.java
+++ Ensyu2_2/src/Ensyu2_2/Ensyu2_2.java
@@ -15,24 +15,24 @@ public class Ensyu2_2 {
 	 * 作成日:2023/04/05
 	 * */
 	public static void main(String[]args) {
-		//変数x,y,zをint型で宣言。
-		int x, y, z;
-		//変数xに10を代入。
-		x = 10;
-		//変数yに20を代入。
-		y = 20;
-		//変数zに30を代入。
-		z = 30;
+		//変数１、２、３をint型で宣言。
+		int FirstIntenger, SecondIntenger, ThirdIntenger;
+		//変数１に10を代入。
+		FirstIntenger = 10;
+		//変数２に20を代入。
+		SecondIntenger = 20;
+		//変数３に30を代入。
+		ThirdIntenger = 30;
 		//標準出力ストリームを用いてそれぞれの表示を行う。
-		//変数xの値を表示。
-		System.out.println("xの値は"+x+"です。");
-		//変数yの値を表示。
-		System.out.println("yの値は"+y+"です。");
-		//変数zの値を表示。
-		System.out.println("zの値は"+z+"です。");
-		//変数x,y,zの合計値を表示。
-		System.out.println("合計は"+(x+y+z)+"です。");
-		//変数x,y,zの平均を表示。
-		System.out.println("平均は"+(x+y+z)/3+"です。");
+		//変数１の値を表示。
+		System.out.println("変数1の値は"+FirstIntenger+"です。");
+		//変数２の値を表示。
+		System.out.println("変数2の値は"+SecondIntenger+"です。");
+		//変数３の値を表示。
+		System.out.println("変数3の値は"+ThirdIntenger+"です。");
+		//３つの変数の合計値を表示。
+		System.out.println("合計は"+(FirstIntenger+SecondIntenger+ThirdIntenger)+"です。");
+		//３つの変数の平均を表示。
+		System.out.println("平均は"+(FirstIntenger+SecondIntenger+ThirdIntenger)/3+"です。");
 	}
 }
