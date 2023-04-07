987c83ddd4e668bbd1929b338a5153c13f05ec84
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-07 15:47:18 +0900

    Ensyu3_17

---
+++ Ensyu3_17/src/Ensyu3_17/Ensyu3_17.java
@@ -0,0 +1,34 @@
+package Ensyu3_17;
+//乱数生成のためにRomdamをインポートする
+import java.util.Random;
+/*
+ * クラス名:Ensyu3_17
+ * 概要:演習3-17
+ * 作成者:城所佑奈
+ * 作成日:2023/04/07
+ * */
+public class Ensyu3_17 {
+	/*
+	 * 関数名:main
+	 * 概要:生成された乱数をもとにグー・チョキ・パーを表示する・
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:城所佑奈
+	 * 作成日:2023/04/07
+	 * */
+	public static void main(String[]args) {
+		//ランダムクラスのインスタンスを初期化する。
+		Random RandomNumber = new Random();
+		//変数HandNumberに１～３の間の乱数を読み込む	
+		int HandNumber = RandomNumber.nextInt(3)+1;
+		//乱数の値で場合わけをする。
+		switch(HandNumber) {
+		//乱数が１のときはグーを表示
+		case 1: System.out.println("グー"); break;
+		//乱数が２のときはチョキを表示
+		case 2: System.out.println("チョキ"); break;
+		//乱数が３のときはパーを表示
+		case 3: System.out.println("パー"); break;
+		}
+	}
+}
