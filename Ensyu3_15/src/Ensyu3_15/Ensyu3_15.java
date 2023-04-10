96494d403fe0dd367cfe41bf1e12b0e4f4f6f015
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 17:51:02 +0900

    変数の命名方法をキャメルケースに修正

--- Ensyu3_15/src/Ensyu3_15/Ensyu3_15.java
+++ Ensyu3_15/src/Ensyu3_15/Ensyu3_15.java
@@ -18,27 +18,27 @@ public class Ensyu3_15 {
 	 * */
 	public static void main(String[]args) {
 		//変数に入力ストリームを渡す。
-		Scanner InputNumber = new Scanner(System.in);
+		Scanner inputNumber = new Scanner(System.in);
 		//変数aの入力を促す。
 		System.out.print("整数a:");
 		//入力された値を読み込む。
-		int IntengerA = InputNumber.nextInt();
+		int inputintegerA = inputNumber.nextInt();
 		
 		//変数bの入力を促す。
 		System.out.print("整数b:");
 		//入力された値を読み込む。
-		int IntengerB = InputNumber.nextInt();
+		int inputintegerB = inputNumber.nextInt();
 		
 		//入力された整数の大小を判定する。
-		if(IntengerA < IntengerB) {
+		if(inputintegerA < inputintegerB) {
 			//値の入れ替え用の仮置き用変数を宣言する。
-			int OnlynowNumber = IntengerA;
-			IntengerA = IntengerB;
-			IntengerB = OnlynowNumber;
+			int onlyNowNumber = inputintegerA;
+			inputintegerA = inputintegerB;
+			inputintegerB = onlyNowNumber;
 		}
 		System.out.println("降順にソートしました。");
-		System.out.println("整数aは"+IntengerA+"です。");
-		System.out.println("整数bは"+IntengerB+"です。");
+		System.out.println("整数aは"+inputintegerA+"です。");
+		System.out.println("整数bは"+inputintegerB+"です。");
 	}
 }
 
