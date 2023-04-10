fdebc44f5a6a1d8740e993231c41fa92a4e963c7
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 14:42:20 +0900

    変数の命名方法を修正

--- Ensyu2_6/src/Ensyu2_6/Ensyu2_6.java
+++ Ensyu2_6/src/Ensyu2_6/Ensyu2_6.java
@@ -18,19 +18,19 @@ public class Ensyu2_6 {
 	* 作成日:2023/04/05
 	* */
 	public static void main (String[] args){
-		//変数InputNumberを用意して、入力ストリームを渡す
-		Scanner InputNumber= new Scanner(System.in);
+		//変数inputNumberを用意して、入力ストリームを渡す
+		Scanner inputNumber= new Scanner(System.in);
 		//三角形の面積を求めることを表示する。
 		System.out.println("三角形の面積を求める");
 		//三角形の高さの値の入力を促す
 		System.out.print("三角形の高さは?：");
-		//double型の変数HeightValueに入力された値を読み込む
-		double HeightValue = InputNumber.nextDouble();
+		//double型の変数heightValueに入力された値を読み込む
+		double heightValue = inputNumber.nextDouble();
 		//三角形の底辺の値の入力を促す
 		System.out.print("三角形の底辺の長さは？：");
-		//double型の変数BaseValueに入力された値を読み込む
-		double BaseValue = InputNumber.nextDouble();
+		//double型の変数baseValueに入力された値を読み込む
+		double baseValue = inputNumber.nextDouble();
 		//入力された値から面積を求め表示する。
-		System.out.println("三角形の面積は"+(HeightValue*BaseValue*0.5)+"です。");
+		System.out.println("三角形の面積は"+(heightValue*baseValue*0.5)+"です。");
 		}
 }
