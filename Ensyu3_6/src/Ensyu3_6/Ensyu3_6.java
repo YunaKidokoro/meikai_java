0719e913a33c0c4308894248e7a59b6c19bb0c52
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 13:45:14 +0900

    訂正版が送れなかったので再コミット

--- Ensyu3_6/src/Ensyu3_6/Ensyu3_6.java
+++ Ensyu3_6/src/Ensyu3_6/Ensyu3_6.java
@@ -18,21 +18,24 @@ public class Ensyu3_6 {
 	 * */
 	public static void main(String[]args) {
 		//変数に入力ストリームを渡す。
-		Scanner InputNumber = new Scanner(System.in);
+		Scanner inputNumber = new Scanner(System.in);
 		//正の整数の値の入力を促す。
 		System.out.print("正の整数を入力してください：");
 		//入力された値を読み込む。
-		int PlusIntenger = InputNumber.nextInt();
-		//入力された値は10の倍数かどうか？
-		if (PlusIntenger > 0)
-			//正の整数かつ10で割り切れるとき
-			if(PlusIntenger%10==0)
+		int plusIntenger = inputNumber.nextInt();
+		//入力された値が正の場合
+		if (plusIntenger > 0)
+			//正の整数かつ10で割り切れる場合
+			if(plusIntenger % 10==0)
+				//その値は10の倍数であることを表示する
 				System.out.println("その値は10の倍数です。");
-			//正の整数でかつ割り切れないとき
+			//正の整数でかつ10で割り切れない場合
 			else
+				//その値は10の倍数でないことを表示する
 				System.out.println("その値は10の倍数ではありません。");	
-		//正の整数でないとき
+		//正の整数でない場合
 		else
+			//正の整数でない値が入力されたことを表示する
 			System.out.println("正でない値が入力されました。");		
 	}
 }
