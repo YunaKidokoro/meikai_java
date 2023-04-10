fd4b93a1a7b55ac6661420d5baae7236d160cd13
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 16:19:46 +0900

    switch文内のインデントと、 ラベルがdefaultの部分のコメントを修正しました。

--- Ensyu3_18/src/Ensyu3_18/Ensyu3_18.java
+++ Ensyu3_18/src/Ensyu3_18/Ensyu3_18.java
@@ -25,32 +25,32 @@ public class Ensyu3_18 {
 		int SeasonNumber = InputNumber.nextInt();
 		//入力の値で場合わけをする。
 		switch(SeasonNumber) {
-		//入力が１のときの季節を表示
-		case 1: System.out.println("冬"); break;
-		//入力が２のときの季節を表示
-		case 2: System.out.println("冬"); break;
-		//入力が３のときの季節を表示
-		case 3: System.out.println("春"); break;
-		//入力が4のときの季節を表示
-		case 4: System.out.println("春"); break;
-		//入力が5のときの季節を表示
-		case 5: System.out.println("春"); break;
-		//入力が6のときの季節を表示
-		case 6: System.out.println("梅雨"); break;
-		//入力が7のときの季節を表示
-		case 7: System.out.println("夏"); break;
-		//入力が8のときの季節を表示
-		case 8: System.out.println("夏"); break;
-		//入力が9のときの季節を表示
-		case 9: System.out.println("秋"); break;
-		//入力が10のときの季節を表示
-		case 10: System.out.println("秋"); break;
-		//入力が11のときの季節を表示
-		case 11: System.out.println("秋"); break;
-		//入力が12のときの季節を表示
-		case 12: System.out.println("冬"); break;
-		//入力された値が13以上の時
-		default: System.out.println("入力内容を確認してください。"); break;
+		 //入力が１のときの季節を表示
+		 case 1: System.out.println("冬"); break;
+		 //入力が２のときの季節を表示
+		 case 2: System.out.println("冬"); break;
+		 //入力が３のときの季節を表示
+		 case 3: System.out.println("春"); break;
+		 //入力が4のときの季節を表示
+		 case 4: System.out.println("春"); break;
+		 //入力が5のときの季節を表示
+		 case 5: System.out.println("春"); break;
+		 //入力が6のときの季節を表示
+		 case 6: System.out.println("梅雨"); break;
+		 //入力が7のときの季節を表示
+		 case 7: System.out.println("夏"); break;
+		 //入力が8のときの季節を表示
+		 case 8: System.out.println("夏"); break;
+		 //入力が9のときの季節を表示
+		 case 9: System.out.println("秋"); break;
+		 //入力が10のときの季節を表示
+		 case 10: System.out.println("秋"); break;
+		 //入力が11のときの季節を表示
+		 case 11: System.out.println("秋"); break;
+		 //入力が12のときの季節を表示
+		 case 12: System.out.println("冬"); break;
+		 //入力された値が0以下、または13以上の時
+		 default: System.out.println("入力内容を確認してください。"); break;
 		}
 	}
 }
