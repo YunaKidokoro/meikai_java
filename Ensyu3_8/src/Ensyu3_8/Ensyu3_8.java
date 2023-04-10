aafac82b186a03c1d7b47018459b86fd6a4357b5
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 14:01:00 +0900

    コメントの修正と、限界値が入力された場合の表示を追加

--- Ensyu3_8/src/Ensyu3_8/Ensyu3_8.java
+++ Ensyu3_8/src/Ensyu3_8/Ensyu3_8.java
@@ -18,26 +18,31 @@ public class Ensyu3_8 {
 	 * */
 	public static void main(String[]args) {
 		//変数に標準出力ストリームを渡す
-		Scanner InputNumber = new Scanner(System.in);
+		Scanner inputNumber = new Scanner(System.in);
 		//点数の入力を求める
 		System.out.print("点数を入力してください：");
-		//入力された値を変数Scoreに読み込む
-		int Score = InputNumber.nextInt();
+		//入力された値を変数scoreValueに読み込む
+		int scoreValue = inputNumber.nextInt();
 		//入力された値の判定を行う
 		//点数が80~100の場合
-		if (Score>=80 && Score<=100)
+		if (scoreValue>=80 && scoreValue<=100)
+			//評価「優」を表示する
 			System.out.println("優");
 		//点数が70~79の場合
-		else if (Score>=70 && Score<=79)
+		else if (scoreValue>=70 && scoreValue<=79)
+			//評価「良」を表示する
 			System.out.println("良");
 		//点数が60~69の場合
-		else if (Score>=60 && Score<=69)
+		else if (scoreValue>=60 && scoreValue<=69)
+			//評価「可」を表示する
 		System.out.println("可");
 		//点数が0~59の場合
-		else if (Score>=0 && Score<=59)
-			System.out.println("可");
-		//入力ミス等の場合
-		else;
-			//System.out.println("入力を確認してください。");
+		else if (scoreValue>=0 && scoreValue<=59)
+			//評価「不可」を表示する
+			System.out.println("不可");
+		//0点以下、または100点以上の場合
+		else
+			//入力された値の確認を促す文を表示する
+			System.out.println("入力を確認してください。");
 	}
 }
