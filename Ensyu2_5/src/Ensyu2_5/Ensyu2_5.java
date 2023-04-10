797bfbb781cba9e2278ba7e5d4c95dd18eb097d5
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 14:40:33 +0900

    変数の命名方法と関数ヘッダを修正

--- Ensyu2_5/src/Ensyu2_5/Ensyu2_5.java
+++ Ensyu2_5/src/Ensyu2_5/Ensyu2_5.java
@@ -10,23 +10,23 @@ import java.util.Scanner;
 public class Ensyu2_5 {
 	/*
 	* 関数名:main
-	* 概要:
+	* 概要:入力された値の合計と平均を表示する
 	* 引数:なし
 	* 戻り値:なし
 	* 作成者:城所佑奈
 	* 作成日:2023/04/05
 	* */
 	public static void main (String[] args){
-	    //変数InputNumberを用意して、入力ストリームを渡す
-		Scanner InputNumber= new Scanner(System.in);
+	    //変数inputNumberを用意して、入力ストリームを渡す
+		Scanner inputNumber= new Scanner(System.in);
 		//xの値の入力を促す文を表示する
 		System.out.print("xの値：");
 		//double型の変数xに入力された値を読み込む
-		double x = InputNumber.nextDouble();
+		double x = inputNumber.nextDouble();
 		//変数ｙに値の入力を促す。
 		System.out.print("yの値：");
 		//double型の変数ｙに入力された値を読み込む
-		double y = InputNumber.nextDouble();
+		double y = inputNumber.nextDouble();
 		//入力されたxとyの合計値を表示する。
 		System.out.println("合計は"+(x+y)+"です。");
 		//入力されたxとyの平均値を表示する。
