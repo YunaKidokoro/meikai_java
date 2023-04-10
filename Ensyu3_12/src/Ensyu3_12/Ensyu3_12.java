b0545c3de2a94f581fe505f2f3738acc593b2018
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 15:08:59 +0900

    変数の命名方法を修正

--- Ensyu3_12/src/Ensyu3_12/Ensyu3_12.java
+++ Ensyu3_12/src/Ensyu3_12/Ensyu3_12.java
@@ -18,32 +18,39 @@ public class Ensyu3_12 {
 	 * */
 	public static void main(String[]args) {
 		//変数に入力ストリームを渡す。
-		Scanner InputNumber = new Scanner(System.in);
+		Scanner inputNumber = new Scanner(System.in);
 		//変数aの入力を促す。
 		System.out.print("整数a:");
 		//入力された値を読み込む。
-		int IntengerA = InputNumber.nextInt();
+		int intengerA = inputNumber.nextInt();
 		//変数bの入力を促す。
 		System.out.print("整数b:");
 		//入力された値を読み込む。
-		int IntengerB = InputNumber.nextInt();
+		int intengerB = inputNumber.nextInt();
 		//変数cの入力を促す。
 		System.out.print("整数c:");
 		//入力された値を読み込む。
-		int IntengerC = InputNumber.nextInt();
+		int intengerC = inputNumber.nextInt();
+
 		//最小値を表す変数MinimumValueを整数aの値で初期化する。
-		int MinimumValue = IntengerA;
-		//MinimumValueと整数bの大きさを比較する。
-		if(MinimumValue > IntengerB)
-			//MinimumValueに整数bの値を代入する。
-			MinimumValue = IntengerB;
-		else;
-		//MinimumValueと整数ｃの大きさを比較する。
-		if(MinimumValue > IntengerC)
-			//MinimumValueに整数ｃの値を代入する。
-			MinimumValue = IntengerC;
-		else;
+		int minimumValue = intengerA;
+		//minimumValueと整数bの大きさを比較する。
+		//整数ｂが仮の最小値よりも小さい場合
+		if(minimumValue > intengerB)
+			//minimumValueに整数bの値を代入する。
+			minimumValue = intengerB;
+		//整数ｂが仮の最小値よりも大きい場合
+		else;	//この場合にはなにも実行しない
+
+		//minimumValueと整数ｃの大きさを比較する。
+		//整数ｃが仮の最小値よりも小さい場合
+		if(minimumValue > intengerC)
+			//minimumValueに整数ｃの値を代入する。
+			minimumValue = intengerC;
+		//整数ｃが仮の最小値よりも大きい場合
+		else;	//この場合にはなにも実行しない
+
 		//最小値を表示する。
-		System.out.println("最小値は"+ MinimumValue +"です。");
+		System.out.println("最小値は"+ minimumValue +"です。");
 	}
 }
