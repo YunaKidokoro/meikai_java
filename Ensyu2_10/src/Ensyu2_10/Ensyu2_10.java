b948aba6ede730d3bcb0967a78567369d3a56e30
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 11:10:52 +0900

    変数の命名方法とクラスヘッダを修正

--- Ensyu2_10/src/Ensyu2_10/Ensyu2_10.java
+++ Ensyu2_10/src/Ensyu2_10/Ensyu2_10.java
@@ -2,33 +2,33 @@ package Ensyu2_10;
 //キーボードからの入力を行うためにjava.util.Scannerをインポート
 import java.util.Scanner;
 /*
- * クラス名
- * 概要
- * 作成者
- * 作成日
+ * クラス名:Ensyu2_10
+ * 概要:演習2-10
+ * 作成者:城所佑奈
+ * 作成日:2023/04/06
  * */
 public class Ensyu2_10 {
 	/*
-	 * 関数名
-	 * 概要
-	 * 引数
-	 * 戻り値
-	 * 作成者
-	 * 作成日
+	 * 関数名:main
+	 * 概要:
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:城所佑奈
+	 * 作成日:2023/04/10
 	 * */
 	public static void main(String[] args) {
 		//変数に入力ストリームを渡す
-		Scanner name  = new Scanner (System.in);
+		Scanner inputName  = new Scanner (System.in);
 		//苗字の入力を促す文の表示
 		System.out.print("姓：");
 		//文字列を読み込む
-		String FamilyName = name.next();
+		String familyName = inputName.next();
 		//名前の入力を促す文の表示
 		System.out.print("名：");
 		//文字列を読み込む
-		String FirstName = name.next();
+		String firstName = inputName.next();
 		//挨拶する文の表示する
-		System.out.println("こんにちは"+FamilyName+FirstName+"さん");
+		System.out.println("こんにちは"+familyName + firstName+"さん");
 	}
 	
 }
