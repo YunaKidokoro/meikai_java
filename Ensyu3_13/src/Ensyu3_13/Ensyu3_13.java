1ca297bd8416fc2462cfca0b61a40b3e1031e3fe
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-10 15:25:35 +0900

    変数の命名方法を修正

--- Ensyu3_13/src/Ensyu3_13/Ensyu3_13.java
+++ Ensyu3_13/src/Ensyu3_13/Ensyu3_13.java
@@ -22,68 +22,68 @@ public class Ensyu3_13 {
 		//変数aの入力を促す。
 		System.out.print("整数a:");
 		//入力された値を読み込む。
-		int intengerA = inputNumber.nextInt();
+		int integerA = inputNumber.nextInt();
 		
 		//変数bの入力を促す。
 		System.out.print("整数b:");
 		//入力された値を読み込む。
-		int intengerB = inputNumber.nextInt();
+		int integerB = inputNumber.nextInt();
 		
 		//変数cの入力を促す。
 		System.out.print("整数c:");
 		//入力された値を読み込む。
-		int intengerC = inputNumber.nextInt();
+		int integerC = inputNumber.nextInt();
 		
 		//中央値を表す変数CenterValueを宣言する。
-		int centerValue = intengerA; //仮に整数aが中央値として初期化する。
+		int centerValue = integerA; //仮に整数aが中央値として初期化する。
 		
 		//入力された値は2つ以上同じ場合
 		//整数aと整数bが同じ値の場合。
-		if (intengerA == intengerB)
+		if (integerA == integerB)
 			//中央値に2つ以上ある値を代入
-			centerValue = intengerA;
+			centerValue = integerA;
 		//整数aと整数cが同じ値の場合
-		else if(intengerA == intengerC)
+		else if(integerA == integerC)
 			//中央値に2つ以上ある値を代入
-			centerValue = intengerC;
+			centerValue = integerC;
 		//整数bと整数cが同じ値の場合
-		else if(intengerB == intengerC)
+		else if(integerB == integerC)
 			//中央値に2つ以上ある値を代入
-			centerValue = intengerB;
+			centerValue = integerB;
 		
 		//入力された値がすべて異なるとき。
 		//最小値を表す変数minimumValueを整数aの値で初期化する。
-		int minimumValue = intengerA;
+		int minimumValue = integerA;
 		//最大値を表す変数maximumValueを整数aの値で初期化する。
-		int maximumValue = intengerA;
+		int maximumValue = integerA;
 		
 		//整数bが整数aよりも大きい場合
-		if (intengerB > maximumValue)		maximumValue = intengerB;
+		if (integerB > maximumValue)		maximumValue = integerB;
 		else;
 		//整数cが整数aよりも大きい場合
-		if (intengerC > maximumValue)		maximumValue = intengerC;
+		if (integerC > maximumValue)		maximumValue = integerC;
 		else;
 		//整数bが整数aよりも小さい場合
-		if (minimumValue > intengerB)		minimumValue = intengerB;
+		if (minimumValue > integerB)		minimumValue = integerB;
 		else;
 		//整数cが整数aよりも小さい場合
-		if (minimumValue > intengerC)		minimumValue = intengerC;
+		if (minimumValue > integerC)		minimumValue = integerC;
 		else;
 		
 		//整数aが最小値でも最大値でもないなら中央値になる。
-		if(intengerA != minimumValue && intengerA != maximumValue)
+		if(integerA != minimumValue && integerA != maximumValue)
 			//この条件を満たす場合の中央値はＡ
-			centerValue = intengerA;
+			centerValue = integerA;
 
 		//整数bが最小値でも最大値でもない場合。
-		else if(intengerB != minimumValue && intengerB != maximumValue)
+		else if(integerB != minimumValue && integerB != maximumValue)
 			//この条件を満たす場合の中央値はB
-			centerValue = intengerB;
+			centerValue = integerB;
 
 		//整数cが最小値でも最大値でもないなら中央値になる。
-		else if(intengerC != minimumValue && intengerC != maximumValue)
+		else if(integerC != minimumValue && integerC != maximumValue)
 			//この条件を満たす場合の中央値はC
-			centerValue = intengerC;
+			centerValue = integerC;
 		//中央値をひょうじする。
 		System.out.println("中央値は"+centerValue+"です。");
 	}
