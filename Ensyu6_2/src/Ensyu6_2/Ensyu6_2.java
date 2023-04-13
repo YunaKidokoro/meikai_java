80eb5241253a7ff9b393ed02d7badde0406df414
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-13 17:53:22 +0900

    Ensyu6_2

---
+++ Ensyu6_2/src/Ensyu6_2/Ensyu6_2.java
@@ -0,0 +1,34 @@
+package Ensyu6_2;
+/*
+ * クラス名:Ensyu6_2
+ * 概要:演習6-2
+ * 作成者:城所佑奈
+ * 作成日:2023/04/13
+ * */
+public class Ensyu6_2 {
+	/*
+	 * 関数名:main
+	 * 概要:要素数が5の配列に対して先頭から順に5~1を代入して表示する
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:城所佑奈
+	 * 作成日:2023/04/13
+	 * */
+	public static void main(String[]args) {
+		//構成要素数が5で、要素型がdouble型の配列を生成する。
+		double []variableArray = new double[5];
+		//インデックスの要素に値を代入していく
+		//カウント数が要素数から1になるまでループする
+		for(int countNumber=variableArray.length; countNumber>0; countNumber--) {
+			//順に値を代入していく
+			variableArray[countNumber - 1] = countNumber;
+		}
+		
+		//全要素の値を表示する
+		//インデックスが0から要素数になるまでループする
+		for(int countNumber=0; countNumber<variableArray.length; countNumber++) {
+			//順に値を表示していく
+			System.out.println("a[" + countNumber + "]=" + variableArray[countNumber]);
+		}
+	}
+}
