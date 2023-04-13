ee53655bc8dfd80c1f91b174986fe12cc316daf7
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-13 11:39:20 +0900

    Ensyu5_4

--- Ensyu5_4/src/Ensyu5_4/Ensyu5_4.java
+++ Ensyu5_4/src/Ensyu5_4/Ensyu5_4.java
@@ -55,7 +55,7 @@ public class Ensyu5_4 {
 		//合計を計算する
 		double totalValue   = firstInteger + secondInteger + thirdInteger;
 		//平均を計算する
-		double averageValue = totalValue/3;
+		double averageValue = totalValue/3.0;
 		
 		//合計と平均を表示をする
 		System.out.println("合計は " + totalValue +"\n平均は"+averageValue);
