b4d73c63688a3d5fe61988bde294240f8805f344
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-20 10:48:20 +0900

    三角形の段数が負の数のときの処理を加えました

--- Ensyu7_7/src/Ensyu7_7/Ensyu7_7.java
+++ Ensyu7_7/src/Ensyu7_7/Ensyu7_7.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 * 作成日:2023/04/19
 * */
 public class Ensyu7_7 {
+	//スキャナークラスのインスタンスを初期化する
+	static Scanner inputNumber = new Scanner(System.in);
 	/*
 	 * メソッド名:putChars
 	 * 概要:引数の文字をn個連続で表示する
@@ -32,6 +34,26 @@ public class Ensyu7_7 {
 		putChars('*',n);
 	}
 	/*
+	 * 関数名:readPlusInt
+	 * 概要:三角形の段数として正の整数値の入力を求め、その値を返す
+	 * 引数:なし
+	 * 戻り値:int型・plusInteger・入力された値(段数)を表す変数
+	 * 作成日:2023/04/20
+	 * */
+	static int readPlusInt() {
+		//正の整数を表す変数を宣言
+		int plusInteger = 0;
+		do {
+			//正の整数値の入力を促す文を表示
+			System.out.print("段数:");
+			//入力された値を読み込む
+			plusInteger = inputNumber.nextInt();
+		//入力が0以下の場合は繰り返す
+		}while(plusInteger <=0);
+		//入力された値を返す
+		return plusInteger;
+	}
+	/*
 	 * 関数名:main
 	 * 概要:左下直角三角形を表示する
 	 * 引数:なし
@@ -39,14 +61,10 @@ public class Ensyu7_7 {
 	 * 作成日:2023/04/19
 	 * */
 	public static void main(String[]args) {
-		//スキャナークラスのインスタンスを初期化する
-		Scanner inputText = new Scanner(System.in);
 		//左下直角三角形を表示することを説明
 		System.out.println("左下直角三角形を表示します");
-		//三角形の段数の入力を促す文を表示
-		System.out.print("段数：");
-		//入力された値を読み込む
-		int inputline = inputText.nextInt();
+		//段数を変数に読み込む
+		int inputline = readPlusInt();
 		//イテレータが0から段数まで変化する間ループする
 		for(int indexNumber=0;indexNumber<=inputline;indexNumber++) {
 			//記号＊を表示
