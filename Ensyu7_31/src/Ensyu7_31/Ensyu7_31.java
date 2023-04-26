cf501c8ff309962cfd1e9c2fe3772ca38e32a1d1
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-26 16:02:55 +0900

    各数値型での乱数を生成してから絶対値を表示するようにしました

--- Ensyu7_31/src/Ensyu7_31/Ensyu7_31.java
+++ Ensyu7_31/src/Ensyu7_31/Ensyu7_31.java
@@ -1,6 +1,6 @@
 package Ensyu7_31;
-//スキャナークラスをインポートする
-import java.util.Scanner;
+//Randomクラスをインポートする
+import java.util.Random;
 /*
 * クラス名:Ensyu7_31
 * 概要:演習7-31
@@ -8,8 +8,8 @@ import java.util.Scanner;
 * 作成日:2023/04/25
 * */
 public class Ensyu7_31 {
-	//スキャナークラスのインスタンスを初期化する
-	static Scanner inputNumber = new Scanner(System.in);
+	//Randomクラスのインスタンスを初期化する
+	static Random randomNumber = new Random();
 
 	/*
 	 * 関数名：absolute
@@ -86,24 +86,7 @@ public class Ensyu7_31 {
 		//絶対値を返す
 		return absoluteNumber;
 	}
-	/*
-	 * 関数名:twoMultiplier
-	 * 概要:2のべき乗の計算をする
-	 * 引数:int型・putOnNumber・乗数を表す
-	 * 戻り値:int型・ResultNumber・2のputOnNumber乗を表す
-	 * 作成日:2023/04/20
-	 * */
-	static int twoMultiplier(int putOnNumber) {
-		//計算結果を表す変数
-		int ResultNumber = 1;
-			//putOnNumber回ループする
-			for(int i=0; i<putOnNumber ; i++) {
-			//2を書ける
-			ResultNumber *= 2;
-		}
-		//計算結果を表示
-		return ResultNumber;
-	}
+
 	/*
 	 * 関数名:main
 	 * 概要:様々な整数型の値の絶対値を求める
@@ -113,52 +96,46 @@ public class Ensyu7_31 {
 	 * 作成者:城所佑奈
 	 */
 	public static void main(String[]argd) {
-		//プログラムの説明文と共に入力を促す
-		System.out.print("入力した値の絶対値を表示します:");
-		//入力された値を読み込む
-		double inputFigure = inputNumber.nextDouble();
+		//プログラムの説明文
+		System.out.println("各数値型で絶対値を表示します");
 		
-		//入力値が整数のとき
-		if(inputFigure%1 ==0) {
-			//入力値がint型の範囲内の場合
-			if(-twoMultiplier(31)<inputFigure && inputFigure<twoMultiplier(31)-1) {
-				//int型で求めたときの表示
-				System.out.println("int型\n"+absolute((int)inputFigure));
-			}
-			//int型の範囲外の場合
-			else{
-				//long型で求めたときの表示
-				System.out.println("long型\n"+absolute((long)inputFigure));
-			}
-		}
-		//入力値が小数点以下を含む場合
-		else {
-			//入力値が負の場合
-			if(inputFigure<0) {
-				//float型の範囲内の場合
-				if(-3.4*10e38<inputFigure && inputFigure<-1.4e-45) {
-					//float型で求めたときの表示
-					System.out.println("float型\n"+absolute((float)inputFigure));
-				}
-				//float型の範囲外の場合
-				else {
-					//double型で求めたときの表示
-					System.out.println("double型\n"+absolute(inputFigure));
-				}
-			}
-			//入力値が正の場合
-			else {
-				//float型の範囲内の場合
-				if(1.4e-45<inputFigure && inputFigure<3.4e38) {
-					//float型で求めたときの表示
-					System.out.println("float型\n"+absolute((float)inputFigure));
-				}
-				//float型の範囲外の場合
-				else {
-					//double型で求めたときの表示
-					System.out.println("double型\n"+absolute(inputFigure));
-				}
-			}
-		}		
+		
+		//int型の乱数を生成する
+		int intInteger = randomNumber.nextInt();
+		//数値型が何か表示
+		System.out.println("int型");
+		//乱数の絶対値を表示
+		System.out.println(absolute(intInteger));
+		
+		
+		//long型の範囲内の乱数を生成
+		long longInteger = randomNumber.nextLong();
+		//数値型が何か表示
+		System.out.println("long型");
+		//乱数の絶対値を表示
+		System.out.println(absolute(longInteger));
+		
+		//float型で乱数を生成するときの下限を表す変数
+		final float LOWER_LIMIT = -10;
+		//float型で乱数を生成するときの上限を表す変数
+		final float HIGHER_LIMIT = 10;
+		//float型
+		float floatInteger = randomNumber.nextFloat(LOWER_LIMIT+HIGHER_LIMIT+2)+LOWER_LIMIT;	
+		//数値型が何か表示
+		System.out.println("float型");
+		//乱数の絶対値を表示
+		System.out.println(absolute(floatInteger));
+		
+		//float型で乱数を生成するときの下限を表す変数
+		final double LOWER_LIMIT_ = -10;
+		//float型で乱数を生成するときの上限を表す変数
+		final double HIGHER_LIMIT_ = 10;
+		//double型
+		double doubleInteger = randomNumber.nextDouble(LOWER_LIMIT_+HIGHER_LIMIT_+2)+LOWER_LIMIT_;
+		//数値型が何か表示
+		System.out.println("double型");
+		//乱数の絶対値を表示
+		System.out.println(absolute(doubleInteger));
+
 	}
 }
