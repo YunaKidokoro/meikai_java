1a2fc71c6f492aab9cca01b13624b53b9394d391
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-27 17:40:29 +0900

    Ensyu9_1

---
+++ Ensyu9_1/src/Ensyu9_1/Ensyu9_1.java
@@ -0,0 +1,76 @@
+package Ensyu9_1;
+
+/*
+ * クラス名:Ensyu9_1
+ * 概要:演習9-1
+ * 作成日:2023/04/26
+ * 作成者:城所佑奈
+ * */
+public class Ensyu9_1 {
+	/*
+	 * 関数名:main
+	 * 概要:HumanClassの配列を生成する
+	 * 引数:なし
+	 * 戻り値:なし
+	 * 作成者:城所佑奈
+	 * 作成日:2023/04/26
+	 * */
+	public static void main(String[]args) {
+		//配列の生成時に要素を初期化する場合であることを表示
+		System.out.println("配列の生成時に要素を初期化");
+		//クラス型の配列を生成
+		ReHumanClass []humanArray = {
+			//配列要素をインスタンスへの参照で初期化する
+			new ReHumanClass("yuna",160,50,0),
+			//配列要素をインスタンスへの参照で初期化する
+			new ReHumanClass("mama",150,45,0),
+			//配列要素をインスタンスへの参照で初期化する
+			new ReHumanClass("papa",170,60,0),
+		};
+		
+		//配列の要素数ぶんループ
+		for(int i=0; i<humanArray.length; i++) {
+			//配列のインデックスを表示
+			System.out.println("x["+i+"]= " 
+			//名前を取得して表示
+			+ humanArray[i].getName() +", "
+			//身長を取得して表示
+			+ humanArray[i].getHeight() + " cm, "
+			//体重を取得して表示
+			+ humanArray[i].getWeight() + " kg, BMI="
+			//BMIを取得して表示
+			+ humanArray[i].getBMI( humanArray[i].getWeight(),humanArray[i].getHeight()));
+		}
+		//見やすくしたいので改行
+		System.out.println();
+		
+		//配列の生成後に各要素にインスタンスの参照を代入する
+		System.out.println("配列の生成後に各要素にインスタンスの参照を代入");
+		//配列を生成する
+		ReHumanClass []arrayAfter ;
+		
+		//配列の各要素にインスタンスの参照を代入していく
+		arrayAfter = new ReHumanClass[] {
+			//インデックス0の配列要素に代入
+			new ReHumanClass("yuna",160,50,0),
+			//インデックス1の配列要素に代入
+			new ReHumanClass("mama",150,45,0),
+			//インデックス2の配列要素に代入
+			new ReHumanClass("papa",170,60,0),};
+		
+		//配列の要素数ぶんループ
+		for(int i=0; i<humanArray.length; i++) {
+			//配列のインデックスを表示
+			System.out.println("y["+i+"]= " 
+			//名前を取得して表示
+			+ arrayAfter[i].getName() +", "
+			//身長を取得して表示
+			+ arrayAfter[i].getHeight() + " cm, "
+			//体重を取得して表示
+			+ arrayAfter[i].getWeight() + " kg, BMI="
+			//BMIを取得して表示
+			+ arrayAfter[i].getBMI( arrayAfter[i].getWeight(),arrayAfter[i].getHeight()));
+		}
+	}
+}
+	
---
+++ Ensyu9_1/src/Ensyu9_1/ReHumanClass.java
@@ -0,0 +1,76 @@
+package Ensyu9_1;
+/*
+ * クラス名:HumanClass
+ * 概要:名前、身長、体重、BMIを扱う
+ * 作成日:2023/04/26
+ * 作成者:城所佑奈
+ * */
+public class ReHumanClass{
+	//名前をインスタンス化
+	private String humanName;
+	//身長(cm)をインスタンス化
+	private int heightValue;
+	//体重(kg)をインスタンス化
+	private int weightValue;
+	//BMIをインスタンス化
+	private float bodyMassIndex;
+	
+	//コンストラクタ
+	ReHumanClass(String humanName, int heightValue, int weightValue, float bodyMassIndex ){
+		//名前を表すフィールドを初期化する
+		this.humanName = humanName;
+		//身長を表すフィールドを初期化する
+		this.heightValue = heightValue;
+		//体重を表すフィールドを初期化する
+		this.weightValue = weightValue;
+		//BMIを表すフィールドを初期化する
+		this.bodyMassIndex = bodyMassIndex;
+	}
+	/*
+	 * 関数名:getName
+	 * 概要:名前を調べる
+	 * 引数:なし
+	 * 戻り値:string型・humanName・その人の名前
+	 * 作成日:2023/04/26
+	 * */
+	String getName() {
+		//その人の名前を返す
+		return humanName;
+	}
+	/*
+	 * 関数名:getHeight
+	 * 概要:身長を調べる
+	 * 引数:なし
+	 * 戻り値:int型・heightValue・身長
+	 * 作成日:2023/04/26
+	 * */
+	int getHeight() {
+		//身長を返す
+		return heightValue;
+	}
+	/*
+	 * 関数名:getWeight
+	 * 概要:身長を調べる
+	 * 引数:なし
+	 * 戻り値:int型・weightValue・体重
+	 * 作成日:2023/04/26
+	 * */
+	int getWeight() {
+		//体重を返す
+		return weightValue;
+	}
+	/*
+	 * 関数名:getBMI
+	 * 概要:BMIを調べる
+	 * 引数:int型・weight・体重
+	 * 		int型・hight ・身長
+	 * 戻り値:float型・bodyMassIndex・BMI
+	 * 作成日:2023/04/26
+	 * */
+	float getBMI(int weight, int hight) {
+		//BMIを計算する(身長はm単位に直す)
+		bodyMassIndex = (weight*10000)/(hight*hight);
+		//BMIを返す
+		return bodyMassIndex;
+	}
+}
