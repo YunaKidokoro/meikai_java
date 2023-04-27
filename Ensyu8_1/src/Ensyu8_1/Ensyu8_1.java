d3c36c04c0a72dba4969fb6c08040570a60f5797
YunaKidokoro <ddt.yn.kidokoro@gmail.com>
2023-04-27 11:19:13 +0900

    フィールドとメソッドを追加しました

--- Ensyu8_1/src/Ensyu8_1/Ensyu8_1.java
+++ Ensyu8_1/src/Ensyu8_1/Ensyu8_1.java
@@ -8,19 +8,23 @@ package Ensyu8_1;
 class HumanClass{
 	//名前をインスタンス化
 	private String humanName;
-	//身長をインスタンス化
+	//身長(cm)をインスタンス化
 	private int heightValue;
-	//体重をインスタンス化
+	//体重(kg)をインスタンス化
 	private int weightValue;
+	//BMIをインスタンス化
+	private float bodyMassIndex;
 	
 	//コンストラクタ
-	HumanClass(String humanName, int heightValue, int weightValue ){
+	HumanClass(String humanName, int heightValue, int weightValue, float bodyMassIndex ){
 		//名前を表すフィールドを初期化する
 		this.humanName = humanName;
 		//身長を表すフィールドを初期化する
 		this.heightValue = heightValue;
 		//体重を表すフィールドを初期化する
 		this.weightValue = weightValue;
+		//BMIを表すフィールドを初期化する
+		this.bodyMassIndex = bodyMassIndex;
 	}
 	/*
 	 * 関数名:getName
@@ -37,7 +41,7 @@ class HumanClass{
 	 * 関数名:getHeight
 	 * 概要:身長を調べる
 	 * 引数:なし
-	 * 戻り値:string型・heightValue・身長
+	 * 戻り値:int型・heightValue・身長
 	 * 作成日:2023/04/26
 	 * */
 	int getHeight() {
@@ -48,12 +52,26 @@ class HumanClass{
 	 * 関数名:getWeight
 	 * 概要:身長を調べる
 	 * 引数:なし
-	 * 戻り値:string型・weightValue・体重
+	 * 戻り値:int型・weightValue・体重
 	 * 作成日:2023/04/26
 	 * */
 	int getWeight() {
 		//体重を返す
 		return weightValue;
+	}
+	/*
+	 * 関数名:getBMI
+	 * 概要:BMIを調べる
+	 * 引数:int型・weight・体重
+	 * 		int型・hight ・身長
+	 * 戻り値:float型・bodyMassIndex・BMI
+	 * 作成日:2023/04/26
+	 * */
+	float getBMI(int weight, int hight) {
+		//BMIを計算する(身長はm単位に直す)
+		bodyMassIndex = (weight*10000)/(hight*hight);
+		//BMIを返す
+		return bodyMassIndex;
 	}
 }
 /*
@@ -73,14 +91,20 @@ public class Ensyu8_1 {
 	 * */
 	public static void main(String[]args) {
 		//yunaのコンストラクタを呼び出す
-		HumanClass yuna = new HumanClass("yuna",160,50);
+		HumanClass yuna = new HumanClass("yuna",160,50, 0);
 		//オブジェクトにメッセージを送る
+		//身長を変数に代入
+		int yunaHeight = yuna.getHeight();
+		//体重を変数に代入
+		int yunaWeight = yuna.getWeight();
 		//だれのデータか表示する
 		System.out.println("名前："+ yuna.getName());
 		//身長を表示する
-		System.out.println("身長："+ yuna.getHeight());
+		System.out.println("身長："+ yunaHeight);
 		//体重を表示する
-		System.out.println("体重："+ yuna.getWeight());
+		System.out.println("体重："+ yunaWeight);
+		//BMIを表示する
+		System.out.println("BMI ："+ yuna.getBMI(yunaWeight,yunaHeight));
 	}
 }
 	
